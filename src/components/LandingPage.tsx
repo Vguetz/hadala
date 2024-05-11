82383d31b0f0e48d65fcf5dd0d3165a1fffcba9e
Vguetz <gomezlucas712@gmail.com>
2024-05-11 17:17:13 -0300

    predeploy

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,9 +3,16 @@ import ImageSlider from './ImageSlider'
 
 export const LandingPage = () => {
   const imageArray = ['/hadalaprueba.jpeg', '/hadalaprueba.jpeg']
+
   return (
     <div>
       <div className='text-center w-full block p-4 justify-center align-middle'>
+        <div className='rounded-sm bg-red-200/30 my-4 p-2 overflow-hidden'>
+          <p className='uppercase font-normal text-muted-foreground animate-marquee'>
+            ¡oferta inaugración! 10% de descuento en tus compras con
+            transferencia bancaria
+          </p>
+        </div>
         <ImageSlider urls={imageArray} />
       </div>
     </div>
